3aa20619c28eb1884874daacaf7c401699268af5
Sabrina Calcina <sabrina.calcina@student.fmf.uni-lj.si>
2020-04-10 20:03:21 +0200

    mini popravek

--- nepremicnine.py
+++ nepremicnine.py
@@ -6,7 +6,7 @@ app = bottle.default_app()
 
 @route('/')
 def index():
-    return bottle.template('zacetna_stran.html')
+    return bottle.template('spletne_strani\zacetna_stran.html')
 
 
 bottle.run(host='localhost', port=8080)

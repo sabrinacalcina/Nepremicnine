417d79790944e35639362be2c7baa398be9509c1
Sabrina Calcina <sabrina.calcina@student.fmf.uni-lj.si>
2020-04-11 22:06:58 +0200

    nadaljevanje prve strani

--- nepremicnine.py
+++ nepremicnine.py
@@ -2,11 +2,15 @@ import definicije
 import bottle
 from bottle import route, run, Response, template
 
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s sumniki
+
 app = bottle.default_app()
 
 @route('/')
 def index():
     return bottle.template('spletne_strani\zacetna_stran.html')
+
 
 
 bottle.run(host='localhost', port=8080)

92fe1dbb889e1c37e12aeb704ddd6427002feb7b
Sabrina Calcina <sabrina.calcina@student.fmf.uni-lj.si>
2020-04-13 13:10:56 +0200

    a

--- nepremicnine.py
+++ nepremicnine.py
@@ -9,15 +9,14 @@ debug(True)
 import hashlib
 
 
-
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s sumniki
-
 
 
 @route("/")
 def main():
     return template("views/zacetna_stran.html")
+
 
 
 

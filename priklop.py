6d3510ac77c9f669141e549f1494d4a7ec986698
DomenFlakusBosilj <domen.4fb@gmail.com>
2020-04-16 17:07:29 +0200

    baza brez uporabnikov

--- priklop.py
+++ priklop.py
@@ -12,6 +12,26 @@ def uvoziSQL(cur, datoteka):
         cur.execute(skripta)
 
 #UVOZ CSV-JA
+def uvoziNepremicnineCSV(cur, tabela):
+    with open('tabele/{0}.csv'.format(tabela), encoding='utf-8') as csvfile:
+        podatki = csv.reader(csvfile)
+        vsiPodatki = [vrstica for vrstica in podatki]
+        glava = vsiPodatki[0]
+        vrstice = vsiPodatki[1:]
+        for i in range(len(vrstice)):
+            agencija = vrstice[i][7]
+            cur.execute("SELECT id FROM agencije WHERE agencija = %s", [agencija])
+            try:
+                vrstice[i][7], = cur.fetchone()
+            except:
+                continue
+        for i in range(len(vrstice)):
+            regija = vrstice[i][8]
+            cur.execute("SELECT id FROM regije WHERE regija = %s", [regija])
+            vrstice[i][8], = cur.fetchone()
+        cur.executemany("INSERT INTO {0} ({1}) VALUES ({2})".format(
+        tabela, ",".join(glava), ",".join(['%s']*len(glava))), vrstice)
+
 def uvoziCSV(cur, tabela):
     with open('tabele/{0}.csv'.format(tabela), encoding='utf-8') as csvfile:
         podatki = csv.reader(csvfile)
@@ -19,27 +39,12 @@ def uvoziCSV(cur, tabela):
         glava = vsiPodatki[0]
         vrstice = vsiPodatki[1:]
         cur.executemany("INSERT INTO {0} ({1}) VALUES ({2})".format(
-            tabela, ",".join(glava), ",".join(['%s']*len(glava))), vrstice)
-
-# def uvoziCSV(cur, tabela):
-#     with open('tabele/{0}.csv'.format(tabela), encoding='utf-8') as csvfile:
-#         podatki = csv.reader(csvfile)
-#         print('helooo')
-#         print(podatki)
-#         vsiPodatki = [vrstica for vrstica in podatki]
-#         glava = vsiPodatki[0]
-#         i = 0
-#         for vrstica in podatki:
-#             cur.execute("INSERT INTO {0} ({1}) VALUES ({2})".format(
-#                 tabela, ",".join(glava), ",".join(['%s']*len(glava))), vrstica)
-#             i += 1
-#             print(i)
-
+        tabela, ",".join(glava), ",".join(['%s']*len(glava))), vrstice)
 
 with psycopg2.connect(database=db, host=host, user=user, password=password) as con:
     cur = con.cursor()
     uvoziSQL(cur, 'tabele.sql')
     uvoziCSV(cur, 'regije')
     uvoziCSV(cur, 'agencije')
-    uvoziCSV(cur, 'nepremicnine')
+    uvoziNepremicnineCSV(cur, 'nepremicnine')
     con.commit()
